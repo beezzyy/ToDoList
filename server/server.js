9dd260cb6a4fb82650011c8308f1eedee3beeaf6
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-05-07 14:09:30 -0400

    testing database in my server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const todoRoutes = require('./routes/todoRoutes');
 const authRoutes = require('./routes/authRoutes');
+const db = require('./db');
 require('dotenv').config();
 
 app.use(cors());
@@ -13,5 +14,13 @@ const PORT = process.env.PORT ?? 8000;
 // get all todos
 app.use('/todos', todoRoutes);
 app.use('/', authRoutes);
+
+db.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error(err.stack);
+  } else {
+    console.log(`Connected to PostgreSQL database at ${res.rows[0].now}`);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
